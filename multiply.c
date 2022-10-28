718984a3ca7fc3aa199faa07b05f4c490586d2c6
Gabriel Sasu <46252462+gasasu@users.noreply.github.com>
2022-10-28 12:55:17 -0700

    Update multiply.c
    Implemented a different solution for the multiply function

--- multiply.c
+++ multiply.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-int main(){
-float x = 23.4;
-float y = 12.4;
+uint8_t multiply(uint8_t num1, uint8_t num2){
+uint8_t sign = 0b10000000;
+uint8_t exp = 0b01111000;
+uint8_t sig = 0b00000111;
+uint8_t s1 = num1 & sign;
+uint8_t s2 = num2 & sign;
+uint8_t exp1 = num1 & exp;
+uint8_t sig1 = num1 & sig;
+uint8_t exp2 = num2 & exp;
+uint8_t sig2 = num2 & sig;
 
-return x*y; }
+uint8_t res_s = s1^s2;
+uint8_t res_exp = exp1 + exp2;
+uint8_t res_sig = sig1 * sig2;
+while (res_sig >= 2){
+    res_exp += 1;
+    res_sig >>= 1;
+}
+return res_s || res_exp || res_sig;
+}
